aa30cd3d99a163c7be922f04c5aa34a3a0d62cb2
Mello <mello+github@ankot.org>
2023-05-19 00:00:01 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iboxstacksops",
-    version="0.7.8",
+    version="0.7.9",
     author="Mello",
     author_email="mello+python@ankot.org",
     description="AWS Infrastructure in a Box - Stacks management program",
