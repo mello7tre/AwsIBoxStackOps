ddc45eed2397061f1ea04bf77a260884f1dec3f5
Mello <mello+github@ankot.org>
2025-04-23 00:00:02 +0200

    iboxstacksops/msg.py, progress on preparing to use Teams

--- iboxstacksops/msg.py
+++ iboxstacksops/msg.py
@@ -13,20 +13,25 @@ else:
 
 def msg_init(stack=None):
     obj = stack.cfg if stack else cfg
+
     try:
         return obj.MSG_CLIENT
     except Exception:
         msg_channel = getattr(obj, "msg_channel", os.environ.get("IBOX_MSG_CHANNEL"))
-        if msg_channel:
-            slack_auth = os.environ.get("IBOX_SLACK_TOKEN")
-            slack_user = os.environ.get("IBOX_SLACK_USER")
-            teams_auth = os.environ.get("IBOX_TEAMS_AUTH")
-            if HAVE_SLACK and slack_auth and slack_user:
-                # For Slack use slack WebClient as msg_client
-                obj.MSG_USER = slack_user
-                obj.MSG_CLIENT = slack.WebClient(token=slack_auth)
-                return obj.MSG_CLIENT
-            elif teams_auth:
-                # For Teams use use request as msg_client
-                # TODO add request url and parameters
-                return request.Request("http://")
+
+        if not msg_channel:
+            return
+
+        slack_auth = os.environ.get("IBOX_SLACK_TOKEN")
+        slack_user = os.environ.get("IBOX_SLACK_USER")
+        teams_auth = os.environ.get("IBOX_TEAMS_AUTH")
+
+        if teams_auth:
+            # For Teams use use request as msg_client
+            # TODO add request url and parameters
+            return request.Request("http://")
+        elif HAVE_SLACK and slack_auth and slack_user:
+            # For Slack use slack WebClient as msg_client
+            obj.MSG_USER = slack_user
+            obj.MSG_CLIENT = slack.WebClient(token=slack_auth)
+            return obj.MSG_CLIENT
