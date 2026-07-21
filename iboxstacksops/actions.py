5d3688025a2f5821ad3472765fe7a7f567cab16e
Mello <mello+github@ankot.org>
2026-07-22 00:00:00 +0200

    EXPRESS Mode - fix
    ===
    When you use express mode, CloudFormation disables rollback by default. If a resource operation fails, CloudFormation does not automatically roll back the changes.
    
    To re-enable rollback when using express mode, set disableRollback to false in the --deployment-config parameter
    ===
    https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cloudformation-express-mode.html#express-mode-rollback
    
    So, "normal" the default value of parameter DisableRollback is False, so
    normally there is no need to specify it, BUT EXPRESS mode se it by
    default to True, so i need to force it to false passing DisableRollback
    param!!
    (Another one bad AWS implementation!!)

--- iboxstacksops/actions.py
+++ iboxstacksops/actions.py
@@ -53,7 +53,7 @@ def _get_action_args(istack):
         us_args["DisableRollback"] = True
 
     if getattr(istack.cfg, "express", False):
-        us_args["DeploymentConfig"] = {"Mode": "EXPRESS", "DisableRollback": True}
+        us_args["DeploymentConfig"] = {"Mode": "EXPRESS", "DisableRollback": False}
 
     return us_args
 
